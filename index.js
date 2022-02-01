ada815d478b41da366b7211ec7940e736ce05cd3
Chloe Peng <chloepeng331@icloud.com>
2022-01-31 19:40:41 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let miles;
+
+function scuberGreetingForFeet(miles){
+  if (miles <= 199) {
+    return 'This one is on me!'
+  } else if (miles >= 2000 && miles <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (miles >= 2501) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+let city;
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip ==='generous'){
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous'){
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
+
+scuberGreetingForFeet(miles);
+ternaryCheckCity(city);
+switchOnCharmFromTip(tip);
